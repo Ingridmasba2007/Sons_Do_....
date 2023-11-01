97c5c7e2d0aea5f11b9c1caf52386128d8e21813
Ingridmasba2007 <127319046+Ingridmasba2007@users.noreply.github.com>
2023-11-01 11:30:34 -0300

    a

--- main.js
+++ main.js
@@ -5,7 +5,4 @@ function somHoraDeAventura (){
 }
     document.querySelector('.tecla_pom').onclick = somHoraDeAventura
 
-function somBen10 (){
-    document.querySelector('#som_tecla_clap').play();
-}
-    document.querySelector('.tecla_clap').onclick = somBen10
+listaDasTeclas[0].onclick = somHoraDeAventura
