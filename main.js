9ba9819bd83f99a385c23fb81c8cf9cc3546f1a1
Ingridmasba2007 <127319046+Ingridmasba2007@users.noreply.github.com>
2023-11-01 11:44:15 -0300

    a

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const listaDasTeclas = document.querySelectorAll('.tecla')
 
+let contador = 0;
+    while (contador < 9){
+        listaDasTeclas.[0].onclick = somHoraDeAventura
+        contador = contador + 1;
+    }
+
 function somHoraDeAventura (){
     document.querySelector('#som_tecla_pom').play();
 }
     document.querySelector('.tecla_pom').onclick = somHoraDeAventura
 
-listaDasTeclas[0, 1, 2, 3, 4, 5, 6, 7, 8].onclick = somHoraDeAventura
+listaDasTeclas[8].onclick = somHoraDeAventura
