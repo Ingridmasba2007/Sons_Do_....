5a6dd53596022f86c7779cfb572e097aa801b300
Ingridmasba2007 <127319046+Ingridmasba2007@users.noreply.github.com>
2023-11-22 11:38:33 -0300

    s

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const listaDasTeclas = document.querySelectorAll('.tecla')
 
 let contador = 0;
     while (contador < listaDasTeclas.length){
-        listaDasTeclas[contador].onclick = som3X3("#som_tecla_splash")
+        listaDasTeclas[contador].onclick = function(){
+            
+            som3X3("#som_tecla_splash")
+        
+        }
         contador = contador + 1;
     }
 
