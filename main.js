d9f15d5e3445ae50c27d0f6c6f9b6ba6d8e53c68
Ingridmasba2007 <127319046+Ingridmasba2007@users.noreply.github.com>
2023-11-22 11:39:01 -0300

    a

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let contador = 0;
     while (contador < listaDasTeclas.length){
         listaDasTeclas[contador].onclick = function(){
             
-            som3X3("#som_tecla_splash")
+            som3X3("#som_tecla_splash");
         
         }
         contador = contador + 1;
