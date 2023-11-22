0247b444063cb4a809254aaad9de973ac9eeafb5
Ingridmasba2007 <127319046+Ingridmasba2007@users.noreply.github.com>
2023-11-22 12:07:23 -0300

    r

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const listaDasTeclas = document.querySelectorAll(".tecla")
 
     while (contador < listaDasTeclas.length){
         const Instrumento = listaDasTeclas[contador].classList
-            console.log(Instrumento)
+        console.log(Instrumento)
         listaDasTeclas[contador].onclick = function(){
             
 
