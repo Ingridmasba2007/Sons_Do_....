6c15b702962688cdb274e9e5b836de52274e17ed
Ingridmasba2007 <127319046+Ingridmasba2007@users.noreply.github.com>
2023-11-22 12:12:09 -0300

    a

--- main.js
+++ main.js
@@ -8,18 +8,16 @@ let contador = 0;
 
 const listaDasTeclas = document.querySelectorAll(".tecla")
 
-    while (contador < listaDasTeclas.length){
-        const Instrumento = listaDasTeclas[contador].classList
-        console.log(Instrumento)
-        
-        listaDasTeclas[contador].onclick = function(){
+while (contador < listaDasTeclas.length){
+    const Instrumento = listaDasTeclas[contador].classList
+    console.log(Instrumento)
+
+    listaDasTeclas[contador].onclick = function(){
             
-
-            som3X3(`#som_${Instrumento}`);
-        
-        }
-        contador = contador + 1;
+    som3X3(`#som_${Instrumento}`);
+         
     }
-
+    contador = contador + 1;
+}
 
    
